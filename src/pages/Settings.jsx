2e5e8658f1f114ae3b4fadc9dd15e638e37115f6
Jeff Anderson <foggeydoughtnut@gmail.com>
2023-04-07 10:03:04 -0600

    settings logout redirects you to the login screen

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,10 +1,26 @@
+import { useNavigate } from "react-router-dom";
 
 const Settings = () => {
+
+  let navigate = useNavigate()
+  const routeChange = (path) => {
+    navigate(path)
+  }
+  const logout = () => {
+    return routeChange("/login")
+  }
+
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h2 className="text-2xl font-bold mt-4">Settings</h2>
       <div className="fixed bottom-8 w-3/4 md:w-2/5 lg:w-1/5">
-        <button className="btn btn-error text-secondary w-full">Logout</button>
+        <button 
+          className="btn btn-error text-secondary w-full"
+          onClick={() => logout()}
+        >
+          Logout
+        </button>
       </div>
     </div>
   )
