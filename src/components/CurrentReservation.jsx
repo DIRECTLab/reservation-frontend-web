319a31eb4e4cf49356c1b4791a960ae4c33dfbca
Ryan Anderson <sonorousduck@gmail.com>
2023-04-10 09:22:42 -0600

    Added logic to grab the information for the current reservation, but no way to test it rn

--- src/components/CurrentReservation.jsx
+++ src/components/CurrentReservation.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+import api from "../api";
 
 const CurrentReservation = ({userId}) => {
+  const [currentReservation, setCurrentReservation] = useState(null)
+
+  const getUpcoming = async () => {
+    return await api.reservation(userId).getCurrent();
+  }
+
+
+  useEffect(() => {
+    let upcoming = getUpcoming();
+    console.log(upcoming);
+    setCurrentReservation(upcoming);
+  }, [])
+
+  useEffect(() => {
+    console.log("Upcoming!" + currentReservation);
+  }, [currentReservation])
+
 
   return (
     <div className="card w-4/5 md:w-3/5 bg-base-100 shadow-xl flex justify-center items-center -z-10">
